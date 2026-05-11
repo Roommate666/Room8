d2783cd458600fc7c06032879edb08e4a2085675
yusufpaypalalbayrak-ux <yusuf.paypal.albayrak@gmail.com>
2026-05-11 14:03:35 +0200

    feat(coupon-detail): About-Box mit business_description + Coupon-Texte aufgehuebscht (Umlaute, bessere Beschreibungen)

--- www/sw.js
+++ www/sw.js
@@ -3,7 +3,7 @@
 // Network First Strategie für schnellere Seitenwechsel
 // ==========================================
 
-var CACHE_NAME = 'room8-v40';
+var CACHE_NAME = 'room8-v41';
 var IMAGE_CACHE = 'room8-images-v1';
 
 // Statische Assets die gecacht werden sollen
